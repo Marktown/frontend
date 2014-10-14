a58a62c6b32c402ea1acf5194d73dd94b64b758b
Jens Bissinger <mail@jens-bissinger.de>
2014-10-14 21:25:04 +0200

    Comment file store interface.

--- models/file_store_interface.go
+++ models/file_store_interface.go
@@ -1,11 +1,24 @@
 package models
 
 type FileStoreInterface interface {
+  // create new file at path
   CreateFile(path string, content []byte) (written bool, err error)
+
+  // update existing file at path
   UpdateFile(path string, content []byte) (written bool, err error)
+
+  // create dir at path
   CreateDir(path string) (created bool, err error)
+
+  // read content of file at path
   ReadFile(path string) (content []byte, err error)
+
+  // list direct child paths within dir at path
   ReadDir(path string) (paths []string, err error)
+
+  // move file or dir at path
   Move(path string, newPath string) (moved bool, err error)
+
+  // delete file or dir at path
   Delete(path string) (deleted bool, err error)
 }
