2b4299573b8af0eef3018b4f965e5e06ddca7383
Jens Bissinger <mail@jens-bissinger.de>
2014-10-14 12:35:14 +0200

    Example implementation of FileHandler.Read.

--- models/file_handler.go
+++ models/file_handler.go
@@ -1,11 +1,23 @@
 package models
 
+import (
+	"io/ioutil"
+)
+
 type FSFileHandler struct {
 }
 
 func (this *FSFileHandler) Create(file *File) {
 	// execute "touch " + file.Path
 	// execute "echo \"" + file.Content "\" > " + file.Path
+}
+
+func (this *FSFileHandler) Read(file *File) (content string, err error) {
+	buffer, err := ioutil.ReadFile(file.Path)
+	if err == nil {
+		content = string(buffer)
+	}
+	return
 }
 
 func (this *FSFileHandler) Update(file *File) {
