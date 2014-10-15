5fa97e7827ef645629344d87ad6f782032c6165a
Jens Bissinger <mail@jens-bissinger.de>
2014-10-15 10:01:45 +0200

    Add abstract file store.

--- backends/file_system/file_system.go
+++ backends/file_system/file_system.go
@@ -3,9 +3,12 @@ package file_system
 import (
 	"io/ioutil"
 	"os"
+
+	"github.com/Marktown/frontend/backends/abstract"
 )
 
 type FileStore struct {
+	abstract.FileStore
 	FilePath string
 }
 
@@ -14,27 +17,7 @@ func (this *FileStore) CreateFile(path string, content []byte) (err error) {
 	return
 }
 
-func (this *FileStore) UpdateFile(path string, content []byte) (err error) {
-	return
-}
-
-func (this *FileStore) CreateDir(path string) (err error) {
-	return
-}
-
 func (this *FileStore) ReadFile(path string) (content []byte, err error) {
 	content, err = ioutil.ReadFile(path)
-	return
-}
-
-func (this *FileStore) ReadDir(path string) (paths []string, err error) {
-	return
-}
-
-func (this *FileStore) Move(path string, newPath string) (err error) {
-	return
-}
-
-func (this *FileStore) Delete(path string) (err error) {
 	return
 }
